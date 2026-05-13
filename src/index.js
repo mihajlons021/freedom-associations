6e1134b3af79c9694808b53d4220b0762a1fea7b
mihajlons <mihajlons@gmail.com>
2026-05-13 16:58:39 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-/* eslint-disable */
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Test</title>
-  </head>
-  <body style="background:red; color:white; font-size:30px; padding:30px;">
-    HTML RADI ✅
-    <div id="root"></div>
-  </body>
-</html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
