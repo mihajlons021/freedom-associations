eb6411d059b5caa5746203b5e2f2b0db4a1bdd0c
mihajlons <mihajlons@gmail.com>
2026-05-13 12:40:42 +0200

    html test

--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Associations</title>
-    <script>
-      window.onerror = function(msg, src, line, col, err) {
-        document.body.style.background = '#fff';
-        document.body.style.color = '#000';
-        document.body.style.padding = '20px';
-        document.body.style.fontSize = '14px';
-        document.body.innerHTML = '<b>ERROR:</b><br>' + msg + '<br><br>Line: ' + line + '<br><br>' + (err ? err.stack : 'no stack');
-        return false;
-      };
-    </script>
+    <title>Test</title>
   </head>
-  <body>
+  <body style="background:red; color:white; font-size:30px; padding:30px;">
+    HTML RADI ✅
     <div id="root"></div>
   </body>
 </html>
